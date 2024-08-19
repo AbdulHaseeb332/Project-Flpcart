b95bbb978b16afac3405824063663b4a5cd87303
Your Name <your-email@example.com>
2024-08-19 07:40:10 -0700

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { NzCascaderOption } from 'ng-zorro-antd/cascader';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  options: NzCascaderOption[] = [
+    {
+      value: 'zhejiang',
+      label: 'Zhejiang',
+      children: [
+        {
+          value: 'hangzhou',
+          label: 'Hangzhou',
+          children: [
+            {
+              value: 'xihu',
+              label: 'West Lake',
+              isLeaf: true
+            }
+          ]
+        },
+        {
+          value: 'ningbo',
+          label: 'Ningbo',
+          isLeaf: true
+        }
+      ]
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu',
+      children: [
+        {
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [
+            {
+              value: 'zhonghuamen',
+              label: 'Zhong Hua Men',
+              isLeaf: true
+            }
+          ]
+        }
+      ]
+    }
+  ];
+
+  nzOptions: NzCascaderOption[] = this.options;
+
+  values: string[] = ['zhejiang', 'hangzhou', 'xihu'];
+
+  onChanges(values: string[]): void {
+    console.log(values, this.values);
+  }
+}
